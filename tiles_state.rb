f0dd7fcfc0f5dc405357af8b959c44dd207e5e44
Bennett Lin <bennettslin@gmail.com>
2013-09-10 22:07:44 -0700

    separate checkScanNotIllegal and returnSonorityAndDyadminoPCs methods from scanSurroundingSpaces method

--- tiles_state.rb
+++ tiles_state.rb
@@ -6,9 +6,9 @@ class TilesState
     @legal_chords, @legal_incompletes = Array.new(2) { [] }
     @rule = rule
     case @rule # number of dyadminos in rack will vary by rules
-      when 0; @rack_num = 6 # folk
-      when 1, 2; @rack_num = 8 # rock, rock with classical
-      when 3, 4; @rack_num = 10 # jazz, jazz with classical
+      when 0; @rack_num = 5 # folk
+      when 1, 2; @rack_num = 6 # rock, rock with classical
+      when 3, 4; @rack_num = 7 # jazz, jazz with classical
       when 5; @rack_num = 5 # octatonic
       when 6; @rack_num = 6 # hexatonic and whole-tone
     else
@@ -143,9 +143,9 @@ class TilesState
     # along with raw chord descriptions (sonority, icp_form, and fake_root)
     legal_move = false # illegal until proven otherwise
     array_of_sonorities = Array.new
-    low_pc, high_pc = @rack_slots[slot_num][:pcs][0], @rack_slots[slot_num][:pcs][1]
+    pcs = @rack_slots[slot_num][:pcs]
     array_of_sonorities =
-      scanSurroundingSpaces(low_pc, low_x, low_y, high_pc, high_x, high_y)
+      scanSurroundingSpaces(pcs, low_x, low_y, high_x, high_y)
     if array_of_sonorities.class == Symbol # this means it's an error, not an array
       print printMessage(array_of_sonorities, nil) unless @testing > 0
       return false
@@ -153,15 +153,15 @@ class TilesState
       this_move_siffr = Array.new
       # siffr is just an acronym for each chord's sonority, icp_form, and fake_root
       array_of_sonorities.each do |son|
-        whether_legal_chord, icp_form, fake_root = checkLegalChord(son)
-        if son.length >= 3 && whether_legal_chord
+        whether_legal_chord, icp_form, fake_root = checkLegalChord(son[0])
+        if son[0].length >= 3 && whether_legal_chord
           legal_move = true
           this_move_siffr <<
-            { sonority: son, icp_form: icp_form, fake_root: fake_root }
-        elsif son.length == 3 && checkLegalIncomplete(son)
-        elsif son.length < 3 # dyad, no action or message
+            { sonority: son[0], icp_form: icp_form, fake_root: fake_root }
+        elsif son[0].length == 3 && checkLegalIncomplete(son[0])
+        elsif son[0].length < 3 # dyad, no action or message
         else
-          print printMessage(:illegal_sonority, son) unless @testing > 0
+          print printMessage(:illegal_sonority, son[0]) unless @testing > 0
           return false
         end
       end
@@ -303,10 +303,11 @@ class TilesState
     return true
   end
 
-  def scanSurroundingSpaces(low_pc, low_x, low_y, high_pc, high_x, high_y)
+  def scanSurroundingSpaces(pcs, low_x, low_y, high_x, high_y)
     # returns EVERY sonority a dyad or larger that is formed by the given dyadmino placement
     # if none are illegal for immediately identifiable physical reasons
     array_of_sonorities = Array.new
+    low_pc, high_pc = pcs[0], pcs[1]
     pcs_to_check = [{ pc: low_pc, x: low_x, y: low_y },
       { pc: high_pc, x: high_x, y: high_y }]
     directions_to_check = [:eastwest, :se_to_nw, :sw_to_ne]
@@ -319,11 +320,12 @@ class TilesState
     end
     pcs_to_check.each do |origin|
       directions_to_check.each do |dir|
-        temp_sonority = [origin[:pc]]
+        temp_sonority = [[origin[:pc], pcs]]
         unless origin[:pc] == high_pc && parallel_axis == dir
           [-1, 1].each do |vector| # checks in both directions
-            temp_pc, temp_x, temp_y = String.new, origin[:x], origin[:y]
-            while temp_pc != :empty
+            temp_x, temp_y = origin[:x], origin[:y]
+            temp_pc = Array.new
+            while temp_pc[0] != :empty
               # establishes that the pc in the temporary container is NOT the empty slot
               # where the dyadmino might go
               case dir
@@ -333,24 +335,38 @@ class TilesState
                   (temp_y - vector) % @board_size
               end
               if temp_x == low_x && temp_y == low_y
-                temp_pc = low_pc
+                temp_pc = [low_pc, pcs]
               elsif temp_x == high_x && temp_y == high_y
-                temp_pc = high_pc
+                temp_pc = [high_pc, pcs]
               else
-                temp_pc = @filled_board_spaces[temp_y][temp_x][0]
+                temp_pc = @filled_board_spaces[temp_y][temp_x]
               end
-              return checkScanNotIllegal(temp_sonority, temp_pc) if
-                !checkScanNotIllegal(temp_sonority, temp_pc)
-              if temp_pc != :empty
+              return checkScanNotIllegal(temp_sonority, temp_pc[0]) if
+                checkScanNotIllegal(temp_sonority, temp_pc[0]).class == Symbol
+              # a returned symbol means it's illegal
+              if temp_pc[0] != :empty
                 vector == -1 ? temp_sonority.unshift(temp_pc) : temp_sonority.push(temp_pc)
               end
             end
           end
         end
-        array_of_sonorities << temp_sonority.sort!.join if temp_sonority.count > 1
+        array_of_sonorities << returnSonorityAndDyadminoPCs(temp_sonority) if temp_sonority.count > 1
       end
     end
     return array_of_sonorities
+  end
+
+  def returnSonorityAndDyadminoPCs(temp_sonority) # called only by scanSurroundingSpaces method
+    # for DEV: refactor so that machine testing does not collect dyadmino pcs
+    # it takes up a lot of extra time
+    single_pc_array, dyadmino_pcs = Array.new(2) { [] }
+    if temp_sonority.count > 1
+      temp_sonority.each do |son|
+        single_pc_array << son[0]
+        dyadmino_pcs << son[1]
+      end
+    end
+    return [single_pc_array.sort.join, dyadmino_pcs]
   end
 
   def checkScanNotIllegal(temp_sonority, temp_pc)
